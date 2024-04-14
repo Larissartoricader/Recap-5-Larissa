5b533eec0f3b4493b3feedd1f859793ce5a1809f
Larissa Sartori-Cader <sartorilarissa.br@gmail.com>
2024-04-14 10:44:12 +0200

    add UserStory 4

--- components/ArtPieceDetails.js
+++ components/ArtPieceDetails.js
@@ -1,7 +1,31 @@
-export default function ArtPieceDetails({}) {
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function ArtPieceDetails({ currentArtPiece }) {
+  const router = useRouter;
+  const { imageSource, name, artist, year, genre, dimensions } =
+    currentArtPiece;
+  const reducedHeight = dimensions.height * 0.5;
+  const reducedWidth = dimensions.width * 0.5;
+
   return (
     <>
-      <h1>ArtPieceDetails</h1>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <h2>{name}</h2>
+      <p>Artist: {artist}</p>
+      <p>Year: {year}</p>
+      <p>Genre: {genre}</p>
+      <Image
+        src={imageSource}
+        alt={`Art ${name} by Artist ${artist}`}
+        height={reducedHeight}
+        width={reducedWidth}
+      />
+      <Link href={"/art-pieces/."}>Back to All Pieces of Art</Link>
     </>
   );
 }
